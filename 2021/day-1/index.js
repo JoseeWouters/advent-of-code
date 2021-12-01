c8835e96c0a8a2718ab69977a795aacd7819e2bb
Josee Wouters <info@joseewouters.nl>
2021-12-01 21:44:09 +0100

    Puzzle 2 Day 1

--- 2021/day-1/index.js
+++ 2021/day-1/index.js
@@ -2,10 +2,20 @@ import input from "./input.js";
 const main = document.querySelector(".main");
 let answer = 0;
 
+// First puzzle
+// for (let i = 0; i < input.length; i++) {
+//     let previousNum = input[i - 1];
+//     let currentNum = input[i];
+//     if (currentNum > previousNum) {
+//         answer += 1;
+//     }
+// }
+
+// Second puzzle
 for (let i = 0; i < input.length; i++) {
-    let previousNum = input[i - 1];
-    let currentNum = input[i];
-    if (currentNum > previousNum) {
+    let previousSum = input[i - 1] + input[i] + input[i + 1];
+    let currentSum = input[i] + input[i + 1] + input[i + 2];
+    if (currentSum > previousSum) {
         answer += 1;
     }
 }
