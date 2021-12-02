62e6f9ca147e710b4edbf8b304de58ae36f384f0
Josee Wouters <info@joseewouters.nl>
2021-12-02 09:02:10 +0100

    Puzzle 2, Day 2

--- 2021/day-2/index.js
+++ 2021/day-2/index.js
@@ -4,22 +4,23 @@ let answer;
 
 let forward = 0;
 let depth = 0;
+let aim = 0;
 
 for (let i = 0; i < input.length; i++ ) {
     for (const [key, value] of Object.entries(input[i])) {
         let amount = parseInt(value, 10)
         if(key === "forward") {
             forward += amount;
+            depth += amount * aim;
         }
         if(key === "down") {
-            depth += amount;
+            aim += amount;
         }
         if(key === "up") {
-            depth -= amount;
+            aim -= amount;
         }
     }    
 }
-
 answer = forward * depth;
 
 main.innerHTML = answer;
