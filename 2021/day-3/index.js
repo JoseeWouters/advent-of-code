6b67a562670ce582a9d33aaa91915ff444a65756
Josee Wouters <info@joseewouters.nl>
2021-12-03 15:55:10 +0100

    WIP Puzzle 2, Day 3

--- 2021/day-3/index.js
+++ 2021/day-3/index.js
@@ -7,24 +7,24 @@ function getPowerConsumption() {
 
 function generateGammaRate() {
     let gammaRate = "";
-    for(let i = 0; i < 12; i++) {
-        gammaRate += getGammaCharacter(i);
+    for (let i = 0; i < 12; i++) {
+        gammaRate += getMostChars(i);
     }
     return parseInt(gammaRate, 2);
 }
 
 function generateEpsilonRate() {
     let epsilonRate = "";
-    for(let i = 0; i < 12; i++) {
-        epsilonRate += getEpsilonCharacter(i);
+    for (let i = 0; i < 12; i++) {
+        epsilonRate += getLeastChars(i);
     }
     return parseInt(epsilonRate, 2);
 }
 
-function getGammaCharacter(index) {
+function getMostChars(index) {
     let zero = 0;
     let one = 0;
-    for (let i = 0; i < input.length; i++ ) {
+    for (let i = 0; i < input.length; i++) {
         let character = input[i].charAt(index);
         character === "0" ? zero += 1 : one += 1;
     }
@@ -33,16 +33,52 @@ function getGammaCharacter(index) {
     return bit;
 }
 
-function getEpsilonCharacter(index) {
+function getLeastChars(index) {
     let zero = 0;
     let one = 0;
-    for (let i = 0; i < input.length; i++ ) {
+    for (let i = 0; i < input.length; i++) {
         let character = input[i].charAt(index);
         character === "0" ? zero += 1 : one += 1;
     }
-    const bit = zero < one ? "0" : "1";
+    const bit = zero <= one ? "0" : "1";
 
     return bit;
 }
 
-main.innerHTML = getPowerConsumption();
+let oxygenRatingValue = "";
+function getOxygenRatingValue(input) {
+    if (input.length === 1 ) {
+        return oxygenRatingValue = input[0];
+    }
+
+    for (let i = 0; i < 12; i++) {
+        const mostZeroesOrOnes = getMostChars(i);
+        input = input.filter(item => item.charAt(i) === mostZeroesOrOnes);
+    }
+    getOxygenRatingValue(input);
+}
+
+let co2ScrubbingValue = "";
+function getco2RatingValue(input) {
+    if (input.length === 1 ) {
+        return co2ScrubbingValue = input[0];
+    }
+
+    for (let i = 0; i < 12; i++) {
+        const mostZeroesOrOnes = getLeastChars(i);
+        input = input.filter(item => item.charAt(i) === mostZeroesOrOnes);
+    }
+    getco2RatingValue(input);
+}
+
+function getLifeSupport() {
+    getOxygenRatingValue(input);
+    getco2RatingValue(input);
+
+    const oxygenValueDecimal = parseInt(oxygenRatingValue, 2);
+    const co2ValueDecimal = parseInt(co2ScrubbingValue, 2);
+
+    return oxygenValueDecimal * co2ValueDecimal;
+}
+
+main.innerHTML = getLifeSupport();
